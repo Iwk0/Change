f2540b786b54bd4d17baaf37bd1bdb02ba0d56d3
Iwk0 <ivomishev@gmail.com>
2014-12-12 16:48:39 +0200

    Last updates

---
+++ app/src/main/java/com/change/GraphActivity.java
@@ -0,0 +1,13 @@
+package com.change;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+public class GraphActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_graph);
+    }
+}
