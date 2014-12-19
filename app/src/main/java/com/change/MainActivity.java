9eaaf817a17a803146e76ce3f931115f26e6c778
Iwk0 <ivomishev@gmail.com>
2014-12-19 16:09:05 +0200

    Last updates

--- app/src/main/java/com/change/MainActivity.java
+++ app/src/main/java/com/change/MainActivity.java
@@ -1,14 +1,16 @@
 package com.change;
 
 import android.app.ActionBar;
+import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.content.res.Configuration;
 import android.os.Bundle;
-import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
@@ -59,18 +61,7 @@ public class MainActivity extends FragmentActivity {
             }
         });
 
-        drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.drawable.ic_drawer,
-                R.string.drawer_open, R.string.drawer_close) {
-            public void onDrawerClosed(View view) {
-                actionBar.setTitle(fragmentTitle);
-                invalidateOptionsMenu();
-            }
-
-            public void onDrawerOpened(View drawerView) {
-                actionBar.setTitle(drawerTitle);
-                invalidateOptionsMenu();
-            }
-        };
+        drawerToggle = new CustomActionBarDrawerToggle(this, drawerLayout);
 
         drawerLayout.setDrawerListener(drawerToggle);
 
@@ -100,6 +91,26 @@ public class MainActivity extends FragmentActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         return drawerToggle.onOptionsItemSelected(item) || super.onOptionsItemSelected(item);
+    }
+
+    private class CustomActionBarDrawerToggle extends ActionBarDrawerToggle {
+
+        public CustomActionBarDrawerToggle(Activity mActivity, DrawerLayout mDrawerLayout) {
+            super(mActivity, mDrawerLayout, new Toolbar(MainActivity.this),
+                    R.string.drawer_open, R.string.drawer_close);
+        }
+
+        @Override
+        public void onDrawerClosed(View view) {
+            actionBar.setTitle(fragmentTitle);
+            invalidateOptionsMenu();
+        }
+
+        @Override
+        public void onDrawerOpened(View drawerView) {
+            actionBar.setTitle(drawerTitle);
+            invalidateOptionsMenu();
+        }
     }
 
     private void selectItem(int position) {
